22083a1d1540c28e633cf2d735722ec13e46fa9b
LawrenceAlpha53 <lawrencellouis@gmail.com>
2025-12-15 05:14:32 +0300

    completed page

--- js/script.js
+++ js/script.js
@@ -20,3 +20,42 @@ let scrollAmount = 0;
 const slideWidth = 320;
 nextBtn.addEventListener('click',()=>{scrollAmount+=slideWidth;slider.scrollTo({left:scrollAmount,behavior:'smooth'});});
 prevBtn.addEventListener('click',()=>{scrollAmount-=slideWidth;if(scrollAmount<0)scrollAmount=0;slider.scrollTo({left:scrollAmount,behavior:'smooth'});});
+
+
+///portifolio js
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImg');
+const modalTitle = document.getElementById('modalTitle');
+const modalDesc = document.getElementById('modalDesc');
+const modalClose = document.getElementById('modalClose');
+
+const galleryCards = document.querySelectorAll('.gallery-card');
+
+galleryCards.forEach(card => {
+  card.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalImg.src = card.querySelector('img').src;
+    modalTitle.textContent = card.getAttribute('data-title');
+    modalDesc.textContent = card.getAttribute('data-desc');
+  });
+});
+
+modalClose.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', (e) => {
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+});
+//contact us
+const contactForm = document.getElementById('contactForm');
+const formStatus = document.getElementById('formStatus');
+
+contactForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  formStatus.textContent = "Thank you! Your message has been sent.";
+  formStatus.style.color = "#00c6ff";
+  contactForm.reset();
+});
